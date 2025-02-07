0e5a72090e677a6f36273dbac4f093d4a6bbd7d1
jakeb-k <jknow30@outlook.com>
2025-02-08 09:23:49 +1000

    [INIT] set up project

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <main class="flex flex-col items-center justify-center h-screen bg-gray-200">
+        <section class=" w-1/3 rounded-2xl py-4 my-6 text-center bg-white shadow-lg">
+            <p>Search Bar</p>
+        </section>
+        <section class="w-1/3 flex flex-col justify-center mx-auto bg-white shadow-lg rounded-lg p-4 text-center h-[50vh]">
+            <p class=""> New Page</p>
+        </section>
+    
+    </main>
+</template>
