fb803af66522aedf2a8bb41124d3443fce70c9b6
jakeb-k <jknow30@outlook.com>
2025-02-08 09:34:37 +1000

    [FEAT] created models and migrations for food and categories

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -27,7 +27,7 @@ defineProps({
             <p>Search Bar</p>
         </section>
         <section class="w-1/3 flex flex-col justify-center mx-auto bg-white shadow-lg rounded-lg p-4 text-center h-[50vh]">
-            <p class=""> New Page</p>
+            <p class="">New Page</p>
         </section>
     
     </main>
