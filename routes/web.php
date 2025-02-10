be02553bda3e11418596f62083a4553f736370e0
jakeb-k <jknow30@outlook.com>
2025-02-10 19:31:06 +1000

    [FIX] fixed clear and updated seeders

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
+    return Inertia::render('Main', [
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
